b02f9c6bd146a67c3f3a25098878d59c4f27298d
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-05 14:56:29 -0800

    Added regex solution

--- Problem-Solving-Polyas/5-look-back-and-refactor.js
+++ Problem-Solving-Polyas/5-look-back-and-refactor.js
@@ -8,3 +8,21 @@
         - Can you think of other ways to refactor?
         - How have other people solved this problem?
  */
+
+// Regex solution
+const charCount = str => {
+    const obj = {};
+    for(let i = 0; i < str.length; i++){
+        let char = str[i].toLowerCase()
+
+        if(/[a-z0-9]/.test(char)) {
+            if(obj[char] > 0) {
+                obj[char]++;
+            } else {
+                obj[char] = 1;
+            }
+        }
+    }
+
+    return obj;
+}
