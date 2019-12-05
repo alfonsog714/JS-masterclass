733f94d500749fc8cf118996778f9a79fbc68a11
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-05 15:02:00 -0800

    Added refactored if statement in step 5

--- Problem-Solving-Polyas/5-look-back-and-refactor.js
+++ Problem-Solving-Polyas/5-look-back-and-refactor.js
@@ -41,4 +41,15 @@ const charCount = str => {
         }
     }
 }
+
+// Refactored if statement
+const charCount = str => {
+    const obj = {};
+    for(let char of str){
+        char = char.toLowerCase();
+        if(/[a-z0-9]/.test(char)) {
+            obj[char] = ++obj[char] || 1;
+        }
+    }
+}
   
