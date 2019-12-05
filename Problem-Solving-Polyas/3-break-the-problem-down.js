761f83b62ee6a5dc3977822b3a2bae9a31432c57
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-05 09:24:59 -0800

    Beginning step 3, breaking down the problem:

---
+++ Problem-Solving-Polyas/3-break-the-problem-down.js
@@ -0,0 +1,45 @@
+/* 
+    Break the problem down
+*/
+
+// =============================================================================================
+// Write a function which takes in a string and returns countws of each character in the string.
+// =============================================================================================
+ 
+charCount("aaaa");
+/*
+    {
+        a: 4
+    }
+ */
+
+charCount("hello");
+/* 
+    {
+        h: 1,
+        e: 1,
+        l: 2,
+        o: 1
+    }
+*/
+
+charCount("Your PIN number is 1234!");
+/* 
+    {
+        1:,
+        2:1,
+        3:1,
+        4:1,
+        b:1,
+        e:1,
+        i:2,
+        m:1,
+        n:2,
+        o:1,
+        p:1,
+        r:2,
+        s:1,
+        u:2,
+        y:1
+    }
+*/
