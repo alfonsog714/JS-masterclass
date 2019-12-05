21d41edbffd8f34d2425ba59f18d6ddc7bd05357
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-05 08:10:05 -0800

    Beginning the problem solving approach section

---
+++ Problem-Solving-Polyas/1-understand-the-problem.js
@@ -0,0 +1,27 @@
+/* 
+    Understand the problem:
+        1. Can I restate the problem in my own words?
+        2. What are the inputs that go into the problem?
+        3. What are the outputs or return that should come from the solution to the problem?
+        4. Can the outputs be determined from the inputs? Basically, do you have enough information to solve the problem?
+        5. How should you label the important pieces of data that are part of the problem?
+*/
+
+// ===============================================================
+// Write a function which takes two numbers and returns their sum.
+// ===============================================================
+
+// 1. Can I restate the problem in my own words?
+`Implement addition.`
+
+// 2. What are the inputs that go into the problem?
+// Ints? floats? What about strings for large numbers?
+`We're expecting two individual integers.`
+
+// 3. What are the outputs that should come from the solution to the problem?
+`We're returning an integer.`
+
+// 4. Can the outputs be determined from the inputs? 
+
+// 5. How should I label the important pieces of data that are a part of the problem?
+`num1 and num2 for the two inputs and sum for the output.`
