189d852f687ef62932d721a9445d41e066d90af8
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-05 09:57:38 -0800

    Finished step 4

--- Problem-Solving-Polyas/4-solve-or-simplify.js
+++ Problem-Solving-Polyas/4-solve-or-simplify.js
@@ -7,3 +7,28 @@
         - Write a simplified solution
         - Incorporate that difficulty back in
 */
+
+const charCount = str => {
+    // make an object to return at the end
+    const counts = {};
+    let lowerCaseString = str.toLowerCase();
+    // loop over the string, for each character
+    for(let i =0; i < str.length; i++) {
+        let char = lowerCaseString[i];
+        
+        // if the char is something else (space, period, etc.) don't do anything
+        if(char === " " || char === "!" || char === "?" || char === ".") {
+            continue;
+        }
+        // if the char is a number/letter AND is a key in object, add 1 to count
+        if(counts[char] > 0) {
+            counts[char]++;
+        }
+        // if the char is a number/letter AND not in the object, add it and set the value to 1
+         else {
+            counts[char] = 1;
+        }
+    }
+    // return an object at the end
+    return counts;
+}
