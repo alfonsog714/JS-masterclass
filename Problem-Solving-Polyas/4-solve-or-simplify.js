37f08901455ba3d29062d0436b75e4828684c2fa
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-05 09:39:55 -0800

    Moving on to step 4, solve or simplify

---
+++ Problem-Solving-Polyas/4-solve-or-simplify.js
@@ -0,0 +1,9 @@
+/* 
+    Solve the problem if you can, else solve a simpler problem (sub-problems of the problem at hand)
+
+    Simplify:
+        - Find the core difficulty in what you're trying to do.
+        - Temporarily ignore that difficulty
+        - Write a simplified solution
+        - Incorporate that difficulty back in
+*/
