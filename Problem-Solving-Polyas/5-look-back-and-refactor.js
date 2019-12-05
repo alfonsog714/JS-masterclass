6142df2f5f398c9c9420e36e1f0438987075f32c
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-05 15:00:00 -0800

    added refactored for loop

--- Problem-Solving-Polyas/5-look-back-and-refactor.js
+++ Problem-Solving-Polyas/5-look-back-and-refactor.js
@@ -26,3 +26,19 @@ const charCount = str => {
 
     return obj;
 }
+
+// Refactored for loop
+const charCount = str => {
+    const obj = {};
+    for(let char of str){
+        char = char.toLowerCase();
+        if(/[a-z0-9]/.test(char)) {
+            if(obj[char] > 0) {
+                obj[char]++;
+            } else {
+                obj[char] = 1;
+            }
+        }
+    }
+}
+  
