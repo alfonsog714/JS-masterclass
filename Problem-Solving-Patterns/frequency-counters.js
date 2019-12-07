fbdb0d5d672a6535c91085720ad41cafeb76b6fa
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-06 18:22:52 -0800

    added naive solution to frequency counter

--- Problem-Solving-Patterns/frequency-counters.js
+++ Problem-Solving-Patterns/frequency-counters.js
@@ -5,9 +5,39 @@
 */
 
 /* 
-    ===============
-    Example Problem
-    ===============
+    =======
+    Problem
+    =======
     Write a function called same, which accepts two arrays.
     The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must be the same.
+
+    ========
+    Examples
+    ========
+    same([1,2,3], [4,1,9]) returns true
+    same([1,2,3], [1,9]) returns false
+    same([1,2,1], [4,4,1]) returns false
 */
+
+/* 
+    =====================
+    Naive O(n^2) solution
+    =====================
+*/
+
+const same = (arr1, arr2) => {
+    if(arr1.length !== arr2.length) {
+        return false;
+    }
+
+    for(let i = 0; i < arr1.length; i++) {
+        let correctIndex = arr2.indexOf(arr1[i] ** 2);
+        if(correctIndex === -1) {
+            return false;
+        }
+        
+        arr2.splice(correctIndex, 1);
+    }
+
+    return true;
+}
