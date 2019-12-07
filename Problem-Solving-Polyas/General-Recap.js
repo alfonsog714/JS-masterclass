0a537ccc28e6f60955851d855f54fa4155052e18
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-06 16:18:41 -0800

    Added the general recap for polyas section

---
+++ Problem-Solving-Polyas/General-Recap.js
@@ -0,0 +1,7 @@
+/* 
+    Step 1. Understand the problem. If you can't restate the problem in your own words, you probably don't understand the question enough.
+    Step 2. Explore or create concrete examples. By this I mean create an example input and what it's output should be, some example edge-cases, etc.
+    Step 3. Pseudocode, or break down the problem.
+    Step 4. Solve or simplify the problem. By simplify I mean create smaller sub-problems of the main problem.
+    Step 5. (Most Important for improving) Look back and refactor the solution.
+*/
