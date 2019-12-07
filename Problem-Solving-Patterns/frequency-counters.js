a6754ed2e580f74ae933eacc16e0c0edb02fa5df
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-06 16:37:25 -0800

    working on frequency pattern

--- Problem-Solving-Patterns/frequency-counters.js
+++ Problem-Solving-Patterns/frequency-counters.js
@@ -3,3 +3,11 @@
 
     This pattern often avoids the need for nested loops.
 */
+
+/* 
+    ===============
+    Example Problem
+    ===============
+    Write a function called same, which accepts two arrays.
+    The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must be the same.
+*/
