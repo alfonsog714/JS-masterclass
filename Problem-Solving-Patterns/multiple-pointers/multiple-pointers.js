ebe0cdfd37eae029c604543aa8309be83e042d97
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-09 15:54:43 -0800

    Added example to multiple pointers

--- Problem-Solving-Patterns/multiple-pointers/multiple-pointers.js
+++ Problem-Solving-Patterns/multiple-pointers/multiple-pointers.js
@@ -4,4 +4,13 @@
     This pattern is very efficient for solving problems with minimal space complexity as well.
 
     Usually using with structures such as arrays, strings, linked lists, etc.
+    
+    ==========
+    An example
+    ==========
+    Write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist.
+
+    sumZero([-3,-2,-1,0,1,2,3]) returns [-3,3]
+    sumZero([-2,0,1,3]) returns undefined
+    sumZero([1]) returns undefined
 */
