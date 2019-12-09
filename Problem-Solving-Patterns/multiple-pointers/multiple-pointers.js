cc231c35d3209eaa3768e6dab07a4eb1206e9d41
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-09 15:58:40 -0800

    added naive solution to multiple pointers

--- Problem-Solving-Patterns/multiple-pointers/multiple-pointers.js
+++ Problem-Solving-Patterns/multiple-pointers/multiple-pointers.js
@@ -14,3 +14,23 @@
     sumZero([-2,0,1,3]) returns undefined
     sumZero([1]) returns undefined
 */
+
+/*
+    ==============
+    Naive Solution
+    ==============
+    Time complexity - O(n^2)
+    Space complexity - O(1)
+*/
+
+function sumZero(arr) {
+    for(let i = 0;i < arr.length; i++) {
+        for(let j = i + 1; j < arr.length; j++) {
+            if(arr[i] + arr[j] === 0) {
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
+
+sumZero([-4,-3,-2,-1,0,1,2,5]);
