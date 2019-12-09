f9a697e1ade4eb9e2f86626158702c88ae2eca85
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-09 12:22:44 -0800

    Added solution provided by instructor

--- Problem-Solving-Patterns/frequency-counter/anagrams.js
+++ Problem-Solving-Patterns/frequency-counter/anagrams.js
@@ -16,6 +16,7 @@
     ("texttwisttime", "timetwisttext") true
 */
 
+// My solution
 const anagram = (str1, str2) =>{
     const word1 = {};
     const word2 = {};
@@ -42,3 +43,30 @@ const anagram = (str1, str2) =>{
 
     return true;
 };
+
+// Solution provided by instructor
+function validAnagram(first, second) {
+    if(first.length !== second.length) {
+        return false;
+    };
+
+    const lookup = {};
+
+    for(let i = 0; i < first.length; i++) {
+        let letter = first[i];
+        //  if letter exists, increment, otherwise set to 1.
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }; 
+
+    for(let i = 0; i < second.length; i++) {
+        let letter = second[i];
+        // can't find letter or letter is zero then it's not an anagram
+        if(!lookup[letter]) {
+            return false;
+        } else {
+            lookup[letter] -= 1;
+        };
+    };
+
+    return true;
+}
