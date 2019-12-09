d8b641991788232cc2fdb21b470fdb9afbe2d0fa
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-09 15:51:37 -0800

    beginning multiple pointers pattern

---
+++ Problem-Solving-Patterns/multiple-pointers/multiple-pointers.js
@@ -0,0 +1,7 @@
+/* 
+    This pattern creates pointers or values that correspond to an index or position and move towards the beginning, end or middle based on a certain condition.
+
+    This pattern is very efficient for solving problems with minimal space complexity as well.
+
+    Usually using with structures such as arrays, strings, linked lists, etc.
+*/
