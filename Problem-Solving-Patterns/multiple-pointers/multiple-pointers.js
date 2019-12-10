94a56404802d11d5c7864bfa01bc40dbf1c156df
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-09 16:48:46 -0800

    Added refactored solution

--- Problem-Solving-Patterns/multiple-pointers/multiple-pointers.js
+++ Problem-Solving-Patterns/multiple-pointers/multiple-pointers.js
@@ -34,3 +34,27 @@ function sumZero(arr) {
 }
 
 sumZero([-4,-3,-2,-1,0,1,2,5]);
+
+/* 
+    ===================
+    Refactored Solution
+    ===================
+    Time complexity - O(n)
+    Space complexity - O(1)
+*/
+
+function sumZero(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while(left < right) {
+        let sum = arr[left] + arr[right];
+        if(sum === 0) {
+            return [arr[left], arr[right]];
+        } else if(sum > 0) {
+            right--;
+        } else {
+            left++;
+        }
+    };
+};
