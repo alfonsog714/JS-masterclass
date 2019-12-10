900f7afe2a168672a6da9528faac883aaf7d1a3c
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-09 18:59:32 -0800

    added refactored solution to sliding-window

--- Problem-Solving-Patterns/sliding-window/sliding-window.js
+++ Problem-Solving-Patterns/sliding-window/sliding-window.js
@@ -36,7 +36,7 @@ function maxSubarraySum(arr, num) {
     let max = -Infinity;
     for(let i = 0; i < arr.length - num + 1; i++) {
         temp = 0;
-        for(let j = 0; j < num; j++) {
+        for(let j = 0; j < num; j++) { 
             temp += arr[i + j];
         };
         if(temp > max) {
@@ -44,4 +44,29 @@ function maxSubarraySum(arr, num) {
         };
     };
     return max;
+};
+
+/* 
+    ===================
+    Refactored Solution
+    ===================
+    Time complexity - O(n)
+*/
+function maxSubarraySum(arr, num) {
+    let maxSum = 0;
+    let tempSum = 0;
+    if(arr.length < num) return null;
+
+    for(let i = 0; i < num; i++) {
+        maxSum += arr[i];
+    };
+    
+    tempSum = maxSum;
+
+    for(let i = num; i < arr.length; i++) {
+        tmepSum = tempSum - arr[i - num] + arr[i];
+        maxSum = Math.max(maxSum, tmepSum);
+    };
+
+    return maxSum;
 };
