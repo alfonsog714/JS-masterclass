0aeade2dc7d4e6d27979547a26920e3d76333627
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-09 18:36:48 -0800

    added instructor's solution to countUniqueValues.js

--- Problem-Solving-Patterns/multiple-pointers/countUniqueValues.js
+++ Problem-Solving-Patterns/multiple-pointers/countUniqueValues.js
@@ -42,3 +42,21 @@ function countUniqueValues(arr) {
 
     return uniqueArray.length;
 };
+
+/* 
+    =====================
+    Instructor's solution
+    =====================
+*/
+function countUniqueValues(arr) {
+    if(arr.length === 0) return 0;
+    let i = 0;
+    for(let j = 1; j < arr.length; j++) {
+        if(arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j];
+        };
+        // console.log(i,j);
+    };
+    return i + 1;
+};
