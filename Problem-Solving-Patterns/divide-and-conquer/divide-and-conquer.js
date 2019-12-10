d0448242063811c071a1ed74dace2b487085d566
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-09 19:25:54 -0800

    added refactored solution

--- Problem-Solving-Patterns/divide-and-conquer/divide-and-conquer.js
+++ Problem-Solving-Patterns/divide-and-conquer/divide-and-conquer.js
@@ -31,3 +31,35 @@ function search(arr, val) {
     };
     return -1;
 };
+
+/*
+    ===================
+    Refactored Solution
+    ===================
+    Binary Search
+    Time Complexity - O(log n)
+
+    THIS WORKS BECAUSE THE ARRAY IS SORTED
+*/
+function search(arr, val) {
+
+    let min = 0;
+    let max = arr.length - 1;
+
+    while(min <= max) {
+        let middle = Math.floor((min + max) / 2);
+        let currentElement = arr[middle];
+
+        if(currentElement < val) {
+            min = middle + 1;
+        }
+        else if(currentElement > val) {
+            max = middle - 1;
+        }
+        else {
+            return middle;
+        };
+    };
+
+    return -1;
+}
