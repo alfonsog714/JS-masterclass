aed6b95dae31532d9c98bc77c931da55356de3df
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-10 20:03:14 -0800

    beginning recursion section

---
+++ Recursion/recursion.js
@@ -0,0 +1,6 @@
+/* 
+    ==================
+    What is recursion?
+    ==================
+    Recursion is a process that calls itself. In regards to programming, that process is usually a function.
+*/
