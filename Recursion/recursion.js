2f09aff827b19defc7687b2549bc704f608ba3f7
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-10 20:38:43 -0800

    added simple example of a recursive function in recursion.js

--- Recursion/recursion.js
+++ Recursion/recursion.js
@@ -14,3 +14,14 @@
     =========
     The condition where the recursion ends. This is the most IMPORTANT concept to understand.
 */
+
+// Recursion example
+function countDown(num) {
+    if(num <= 0) {
+        console.log("All done!");
+        return;
+    };
+    console.log(num)
+    num--;
+    countDown(num);
+}
