a09d9fe18a386b12fba0dece687670d853182e36
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-10 20:35:41 -0800

    added more notes to recursion.js

--- Recursion/recursion.js
+++ Recursion/recursion.js
@@ -3,4 +3,14 @@
     What is recursion?
     ==================
     Recursion is a process that calls itself. In regards to programming, that process is usually a function.
+
+    ============================
+    How recursive functions work
+    ============================
+    Invoke the same function with a different input until you reach your base case.
+
+    =========
+    Base Case
+    =========
+    The condition where the recursion ends. This is the most IMPORTANT concept to understand.
 */
