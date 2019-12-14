59c9b9dcf82f6424c94049eab1d18553e61b82d6
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-14 08:29:51 -0800

    forgot what i did

--- Problem-Solving-Patterns/frequency-counter/frequency-counters.js
+++ Problem-Solving-Patterns/frequency-counter/frequency-counters.js
@@ -64,7 +64,7 @@ const same = (arr1, arr2) => {
         frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
     };
 
-    for(let key in frequencyCounter1){
+    for(let key in frequencyCounter1) {
         if(!(key ** 2 in frequencyCounter2)) {
             return false;
         }
