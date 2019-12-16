4a80d88970e2b1450ea81ccfe30bd2be8ffcc227
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-16 14:03:55 -0800

    added big o analysis of bubble sort

--- Sorting-Algorithms/bubble-sort/bubble-sort-overview.js
+++ Sorting-Algorithms/bubble-sort/bubble-sort-overview.js
@@ -9,4 +9,10 @@
     What is Bubble Sort?
     ====================
     A sorting algorithm where the largest values bubble up to the top  
+
+    =============
+    Big O runtime
+    =============
+    - O(n^2)
+    - If the array is nearly sorted, the runtime can be O(n) using the "noSwaps" version of bubble sort
 */
