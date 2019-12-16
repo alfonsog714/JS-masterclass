7c752effaf59c169dbc220a041fd7fa594784410
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-16 13:35:23 -0800

    beginning bubble sort

---
+++ Sorting-Algorithms/bubble-sort/bubble-sort-overview.js
@@ -0,0 +1,12 @@
+/* 
+    ===========
+    Bubble Sort
+    ===========
+    - Not that efficient
+    - Not commonly used
+
+    ====================
+    What is Bubble Sort?
+    ====================
+    A sorting algorithm where the largest values bubble up to the top
+*/
