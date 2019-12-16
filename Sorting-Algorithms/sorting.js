3e9c962f4759c8c03ec5db18282985c0669a3856
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-16 12:44:35 -0800

    beginning the sorting algorithms section

---
+++ Sorting-Algorithms/sorting.js
@@ -0,0 +1,13 @@
+/* 
+    ================
+    What is sorting?
+    ================
+    Sorting is the process of rearranging the items in a collection (e.g. an array) so that the items are in some kind of order.
+
+    ========
+    Examples
+    ========
+    - Sorting numbers from smallest to largest
+    - Sorting names alphabetically
+    - Sorting movies based on release year
+*/
