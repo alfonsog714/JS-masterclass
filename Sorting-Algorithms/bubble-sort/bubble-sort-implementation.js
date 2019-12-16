81a7b97bbdc15b359c696ab9122274d67badf9b8
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-16 13:53:59 -0800

    added optimized version of bubble sort

--- Sorting-Algorithms/bubble-sort/bubble-sort-implementation.js
+++ Sorting-Algorithms/bubble-sort/bubble-sort-implementation.js
@@ -21,12 +21,35 @@ function bubbleSort(arr){
                 // SWAP!
                 var temp = arr[j];
                 arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j+1] = temp;  
             };
         }
     };
 
     return arr;
-}
+};
+
+/* 
+    =================
+    Optimized version
+    =================
+    - i begins at the end of the array, allowing us to use it in the statement for how long the inner loop will run.
+    - j stops before the last sorted item
+*/
+
+function bubbleSort(arr){
+    for(var i = arr.length; i > 0; i--) {
+        for(var j = 0; j < i - 1; j++) {
+            if(arr[j] > arr[j+1]) {
+                // SWAP!
+                var temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;  
+            };
+        }
+    };
+
+    return arr;
+};
 
 bubbleSort([37,45,29,8]);
