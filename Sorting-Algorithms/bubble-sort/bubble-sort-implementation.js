784928d777ac90f39b454f7efd842ea6bbe2a375
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-16 13:43:50 -0800

    added pseudocode for bubble sort

---
+++ Sorting-Algorithms/bubble-sort/bubble-sort-implementation.js
@@ -0,0 +1,9 @@
+/* 
+    ==========
+    Pseudocode
+    ==========
+    - Start looping with a variable called i at the end of the array towards the beginning
+    - Start an inner loop with a variable called j from the beginning until i - 1
+    - If arr[j] is greater than arr[j+1], swap those two values!
+    - Return the sorted array
+*/
