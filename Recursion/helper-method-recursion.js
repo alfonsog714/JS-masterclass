14dafeca8bbcf3552e4c8bb42d58bf533aaf29df
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-16 10:46:49 -0800

    added pure recursive version of collectOddValues

--- Recursion/helper-method-recursion.js
+++ Recursion/helper-method-recursion.js
@@ -46,3 +46,22 @@ function collectOddValues(arr) {
 
     return result;
 };
+
+/* 
+    ======================================
+    Pure recursion way of collectOddValues
+    ======================================
+*/
+
+function collectOddValues(arr) {
+    let newArr = [];
+
+    if(arr.length === 0) return newArr;
+
+    if(arr[0] % 2 !== 0) {
+        newArr.push(arr[0]);
+    };
+
+    newArr = newArr.concat(collectOddValues(arr.slice(1)));
+    return newArr;
+};
