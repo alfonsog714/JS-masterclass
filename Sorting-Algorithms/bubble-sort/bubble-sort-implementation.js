3de9f22f6ec69c98f4e209e7a9a4a37e68575383
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-16 13:57:22 -0800

    es6 version of bubble sort added

--- Sorting-Algorithms/bubble-sort/bubble-sort-implementation.js
+++ Sorting-Algorithms/bubble-sort/bubble-sort-implementation.js
@@ -29,6 +29,7 @@ function bubbleSort(arr){
     return arr;
 };
 
+bubbleSort([37,45,29,8]);
 /* 
     =================
     Optimized version
@@ -53,3 +54,25 @@ function bubbleSort(arr){
 };
 
 bubbleSort([37,45,29,8]);
+
+/* 
+    ==========================
+    ES6 version of bubble sort
+    ==========================
+*/
+
+function bubbleSort(arr) {
+    const swap = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+
+    for(let i = arr.length; i > 0; i--) {
+        for(let j = 0; j < i - 1; j++) {
+            if(arr[j] > arr[j+1]) {
+                swap(arr, j, j+1);
+            };
+        };
+    };
+
+    return arr;
+}
