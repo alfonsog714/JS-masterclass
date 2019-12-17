5637ad4aaf3b6746dc1f956cbb9a284a1672cae8
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-17 15:29:47 -0800

    added implementation if pivot helper

--- Sorting-Algorithms/intermediate-sorting-algorithms/quick-sort/pivot-helper.js
+++ Sorting-Algorithms/intermediate-sorting-algorithms/quick-sort/pivot-helper.js
@@ -42,3 +42,27 @@
     - Swap the starting element (i.e. the pivot) with the pivot index
     - Return the pivot index
  */
+
+function pivot(arr, start = 0, end = arr.length + 1) {
+    function swap(array, i, j) {
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    };
+
+    let pivot = arr[start];
+    let swapIdx = start;
+
+    for(let i = start + 1; i < arr.length; i++) {
+        if(pivot > arr[i]) {
+            swapIdx++;
+            swap(arr, swapIdx, i);
+        };
+    };
+
+    swap(arr, start, swapIdx);
+
+    return swapIdx;
+};
+
+pivot([4,8,2,1,5,7,6,3]); // should return 3
