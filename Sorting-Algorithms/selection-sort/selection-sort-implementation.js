c4e5360781e94b243f3a0d085f13a2d9b6b24e84
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-17 09:51:00 -0800

    added implementation of selection sort

--- Sorting-Algorithms/selection-sort/selection-sort-implementation.js
+++ Sorting-Algorithms/selection-sort/selection-sort-implementation.js
@@ -8,3 +8,25 @@
     - If the "minimum" is not the value (index) you initially began with, swap the two values.
     - Repeat this with the next element until the array is sorted.
 */
+
+function selectionSort(arr) {
+    for(let i = 0; i < arr.length;i++) {
+        let lowest = i;
+
+        for(let j = i + 1; j < arr.length; j++) {
+            if(arr[j] < arr[lowest]) {
+                lowest = j;
+            };
+        };
+        
+        if(i !== lowest) {
+            let temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        };
+    };
+
+    return arr;
+};
+
+selectionSort([34,22,10,19,17]);
