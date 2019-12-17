67b1f61009e1dd131b8962ba66e876536988daee
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-17 10:41:43 -0800

    beginning merge sort

---
+++ Sorting-Algorithms/intermediate-sorting-algorithms/merge-sort/merge-sort-overview.js
@@ -0,0 +1,9 @@
+/* 
+    ==========
+    Merge Sort
+    ==========
+    - Combination of two things - merging and sorting
+    - Exploits the fact that arrays of 0 or 1 element are always sorted
+    - Works by decomposing an array into smaller arrays of 0 or 1 elements, then building up a newly sorted array
+
+*/
