901a16ba57ef62c5cf8748900fbad1ef7228a74e
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-17 12:04:36 -0800

    added implementation of the merge array helper function

--- Sorting-Algorithms/intermediate-sorting-algorithms/merge-sort/merge-arrays.js
+++ Sorting-Algorithms/intermediate-sorting-algorithms/merge-sort/merge-arrays.js
@@ -15,3 +15,33 @@
     -       If the value in the first array is larger than the value in the second array, push the value in the second array into our results and move on to the next value in the second array.
     -       Once we exhaust one array, push in all remaining values from the other array.
 */
+
+function merge(arr1, arr2) {
+    let results = [];
+    let i = 0;
+    let j = 0;
+
+    while(i < arr1.length && j < arr2.length) {
+        if(arr2[j] > arr1[i]) {
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j]);
+            j++;
+        }
+    };
+
+    while(i < arr1.length) {
+        results.push(arr1[i]);
+        i++;
+    };
+
+    while(j < arr2.length) {
+        results.push(arr2[j]);
+        j++;
+    };
+    
+    return results;
+};
+
+merge([1,10,50], [2,14,99,100]);
