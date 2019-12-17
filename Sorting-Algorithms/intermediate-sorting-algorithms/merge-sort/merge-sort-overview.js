14c1ae5b278ad70840bc6b5959e55cd7bb7f4aea
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-17 10:45:11 -0800

    added merge sort visualization

--- Sorting-Algorithms/intermediate-sorting-algorithms/merge-sort/merge-sort-overview.js
+++ Sorting-Algorithms/intermediate-sorting-algorithms/merge-sort/merge-sort-overview.js
@@ -6,4 +6,15 @@
     - Exploits the fact that arrays of 0 or 1 element are always sorted
     - Works by decomposing an array into smaller arrays of 0 or 1 elements, then building up a newly sorted array
 
+    =============
+    Visualization
+    =============
+
+                [8, 3, 5, 4, 7, 6, 1, 2]
+            [8, 3, 5, 4]        [7, 6, 1, 2]
+         [8, 3]      [5, 4]     [7, 6]   [1, 2]
+            [8] [3] [5] [4] [7] [6] [1] [2]
+            [3, 8]  [4, 5]  [6, 7]  [1, 2]
+             [3, 4, 5, 8]    [1, 2, 6, 7]
+               [1, 2, 3, 4, 5, 6, 7, 8]
 */
