2074cf3a5cc7120e21d25832f420b53b593bf120
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-17 10:18:33 -0800

    added big o notation for insertion sort

--- Sorting-Algorithms/insertion-sort/insertion-sort-overview.js
+++ Sorting-Algorithms/insertion-sort/insertion-sort-overview.js
@@ -3,4 +3,9 @@
     Insertion Sort
     ==============
     - Builds up the sort by gradually creating a larger left portion which is always sorted.
+
+    =============
+    Big O Runtime
+    =============
+    - O(n^2)
 */
