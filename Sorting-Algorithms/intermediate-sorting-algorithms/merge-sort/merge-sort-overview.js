6f01eec77d969a8bf5996e1f6464b4b70e2696e2
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-17 14:24:33 -0800

    added big o notation of merge sort

--- Sorting-Algorithms/intermediate-sorting-algorithms/merge-sort/merge-sort-overview.js
+++ Sorting-Algorithms/intermediate-sorting-algorithms/merge-sort/merge-sort-overview.js
@@ -17,4 +17,11 @@
             [3, 8]  [4, 5]  [6, 7]  [1, 2]
              [3, 4, 5, 8]    [1, 2, 6, 7]
                [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+    ===================
+    Big O of Merge Sort
+    ===================
+    - Best and worst case of merge sort is O(n log n)
+    - The space complexity is O(n)
 */
