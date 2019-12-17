86ea2c41bd157dc8806e42383b89be23193df1c0
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-17 09:57:42 -0800

    added selection sort big o

--- Sorting-Algorithms/selection-sort/selection-sort-overview.js
+++ Sorting-Algorithms/selection-sort/selection-sort-overview.js
@@ -4,4 +4,10 @@
     ==============
     - Similar to bubble sort
     - Places small values into sorted position instead of the big values
+
+    =============
+    Big O Runtime
+    =============
+    - O(n^2)
+    - Potentially a better algorithm to use if for some reason you want to minimize how many swaps you do.
 */
