a2664c173fc937d011aea36d0d74d34090b7347e
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-17 10:14:26 -0800

    added implementation of insertion sort

--- Sorting-Algorithms/insertion-sort/insertion-sort-implementation.js
+++ Sorting-Algorithms/insertion-sort/insertion-sort-implementation.js
@@ -7,3 +7,19 @@
     - Continue to the next element and if it is in the incorrect order, iterate through the sorted portion(i.e the left side) to place the element in the correct place.
     - Repeat until the array is sorted
 */
+
+function insertionSort(arr) {
+    for(let i = 1; i < arr.length; i++) {
+        let currentVal = arr[i];
+
+        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--) { // notice I used var here instead of let. This is so I can reference j outside of the loop.
+            arr[j+1] = arr[j];
+        };
+
+        arr[j+1] = currentVal;
+    };
+
+    return arr;
+};
+
+insertionSort([2,1,9,76,4]);
