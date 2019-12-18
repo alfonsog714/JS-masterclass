2b2922b12a5d92c3c319537f32b6f860e1b2bb2a
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-18 09:36:46 -0800

    fixed pivot code

--- Sorting-Algorithms/intermediate-sorting-algorithms/quick-sort/pivot-helper.js
+++ Sorting-Algorithms/intermediate-sorting-algorithms/quick-sort/pivot-helper.js
@@ -43,7 +43,7 @@
     - Return the pivot index
  */
 
-function pivot(arr, start = 0, end = arr.length + 1) {
+function pivot(arr, start = 0, end = arr.length - 1) {
     function swap(array, i, j) {
         let temp = array[i];
         array[i] = array[j];
@@ -53,7 +53,7 @@ function pivot(arr, start = 0, end = arr.length + 1) {
     let pivot = arr[start];
     let swapIdx = start;
 
-    for(let i = start + 1; i < arr.length; i++) {
+    for(let i = start + 1; i < end; i++) {
         if(pivot > arr[i]) {
             swapIdx++;
             swap(arr, swapIdx, i);
--- Sorting-Algorithms/intermediate-sorting-algorithms/quick-sort/quick-sort-implementation.js
+++ Sorting-Algorithms/intermediate-sorting-algorithms/quick-sort/quick-sort-implementation.js
@@ -6,3 +6,29 @@
     - When the helper returns to you the updated pivot index, recursively call the pivot helper on the subarray to the left of that index, and on the subarray to the right of that index.
     - Your base case occurs when you consider a subarray with less than 2 elements
 */
+
+function pivot(arr, start = 0, end = arr.length - 1) {
+    function swap(array, i, j) {
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    };
+
+    let pivot = arr[start];
+    let swapIdx = start;
+
+    for(let i = start + 1; i <= end; i++) {
+        if(pivot > arr[i]) {
+            swapIdx++;
+            swap(arr, swapIdx, i);
+        };
+    };
+
+    swap(arr, start, swapIdx);
+
+    return swapIdx;
+};
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    let pivotIndex = pivot(arr, left, right);
+};
