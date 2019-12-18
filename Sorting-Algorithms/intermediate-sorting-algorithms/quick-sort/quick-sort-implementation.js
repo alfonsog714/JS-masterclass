fdd911644dc0007c45fa2a673434388c257931be
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-18 09:33:29 -0800

    added quicksort pseudocode

---
+++ Sorting-Algorithms/intermediate-sorting-algorithms/quick-sort/quick-sort-implementation.js
@@ -0,0 +1,8 @@
+/* 
+    ====================
+    Quicksort Pseudocode
+    ====================
+    - Call the pivot helper on the array
+    - When the helper returns to you the updated pivot index, recursively call the pivot helper on the subarray to the left of that index, and on the subarray to the right of that index.
+    - Your base case occurs when you consider a subarray with less than 2 elements
+*/
