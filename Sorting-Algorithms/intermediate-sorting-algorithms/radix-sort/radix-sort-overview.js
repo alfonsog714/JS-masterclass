c1eee348d625d631405267a3fb2c3c34d9d218eb
Alfonso <alfonso.garcia2324@gmail.com>
2019-12-19 10:16:18 -0800

    added more to radix sort

--- Sorting-Algorithms/intermediate-sorting-algorithms/radix-sort/radix-sort-overview.js
+++ Sorting-Algorithms/intermediate-sorting-algorithms/radix-sort/radix-sort-overview.js
@@ -4,4 +4,7 @@
     ==========
     - Radix sort is a special sorting algorithm that works on lists of numbers
     - Usually used with binary numbers
+    - Never makes comparisons between elements!
+    - Exploits the fact that information about the size of a number is encoded in the number of digits
+    - More digits means a bigger number!
 */
